922e19a8169447a1bc50f466eaf58c0d645a65fb
Clement Cordeiro <clement.cordeiro@gmail.com>
2022-11-14 16:59:02 +0100

    games controller second method ok

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,3 @@
-require "json"
 require "open-uri"
 
 class GamesController < ApplicationController
@@ -8,15 +7,10 @@ class GamesController < ApplicationController
   end
 
   def score
-    # url = "https://wagon-dictionary.herokuapp.com"
-    # user_serialized = URI.open(url).read
-    # result = JSON.parse(user_serialized)
 
-    # puts "#{result[""]} - #{user["bio"]}"
     answer = params[:answer].chars
     game_all_letters = params[:letters].split
     @result = "Well done"
-    raise
 
     answer.each do |answer_letter|
       if game_all_letters.include?(answer_letter)
@@ -27,10 +21,10 @@ class GamesController < ApplicationController
       end
     end
 
-
+    url = "https://wagon-dictionary.herokuapp.com/#{params[:answer]}"
+    user_serialized = URI.parse(url).open.read
+    result = JSON.parse(user_serialized)
+    @result = "Not a valid word" unless result["found"]
   end
-
-
-
 
 end
